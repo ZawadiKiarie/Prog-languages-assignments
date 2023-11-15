6b9ab41c484ea49bf7022156d0cd5478395538ec
unknown <133970406+ZawadiKiarie@users.noreply.github.com>
2023-11-15 21:22:32 +0300

    Modify simple calculator to use try and except

---
+++ simple-calc-try-except.py
@@ -0,0 +1,14 @@
+# kiarie zawadi muthoni 
+# sct211-0462/2022
+
+try:
+  num1 = int(input("Enter the first number: "))
+  num2 = int(input("Enter the second number: "))
+  result = num1 + num2
+  print(f"The sum of the two numbers is {result}")
+except ValueError:
+  print('Please enter a number')
+
+
+
+
